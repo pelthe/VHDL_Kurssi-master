bb43d6364d695e59e0bd353a7ebc3127f2d74217
pelthe <54885089+pelthe@users.noreply.github.com>
2019-10-09 09:29:00 +0300

    added a new project-folder for Labs 4 & 5

---
+++ LABS_4_5/LABS_4_5.srcs/sources_1/new/LABS_4_5.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09.10.2019 09:24:11
+-- Design Name: 
+-- Module Name: LABS_4_5 - rtl
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity LABS_4_5 is
+--  Port ( );
+end LABS_4_5;
+
+architecture rtl of LABS_4_5 is
+
+begin
+
+
+end rtl;
