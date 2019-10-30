0048f34d93efeec4a39921390e6e526d5b5e965f
pelthe <54885089+pelthe@users.noreply.github.com>
2019-10-30 10:31:21 +0200

    TB to ClockGen

--- LABS_4_5/LABS_4_5.sim/sim_1/behav/xsim/xsim.dir/Tb_clock_divider_behav/obj/xsim_1.c
+++ LABS_4_5/LABS_4_5.sim/sim_1/behav/xsim/xsim.dir/Tb_clock_divider_behav/obj/xsim_1.c
@@ -44,20 +44,22 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
+extern void execute_13(char*, char *);
+extern void execute_14(char*, char *);
+extern void execute_9(char*, char *);
 extern void execute_10(char*, char *);
 extern void execute_11(char*, char *);
-extern void execute_8(char*, char *);
-extern void execute_9(char*, char *);
+extern void execute_12(char*, char *);
 extern void transaction_0(char*, char*, unsigned, unsigned, unsigned);
 extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[6] = {(funcp)execute_10, (funcp)execute_11, (funcp)execute_8, (funcp)execute_9, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
-const int NumRelocateId= 6;
+funcp funcTab[8] = {(funcp)execute_13, (funcp)execute_14, (funcp)execute_9, (funcp)execute_10, (funcp)execute_11, (funcp)execute_12, (funcp)transaction_0, (funcp)vhdl_transfunc_eventcallback};
+const int NumRelocateId= 8;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/Tb_clock_divider_behav/xsim.reloc",  (void **)funcTab, 6);
-	iki_vhdl_file_variable_register(dp + 2840);
-	iki_vhdl_file_variable_register(dp + 2896);
+	iki_relocate(dp, "xsim.dir/Tb_clock_divider_behav/xsim.reloc",  (void **)funcTab, 8);
+	iki_vhdl_file_variable_register(dp + 4088);
+	iki_vhdl_file_variable_register(dp + 4144);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
--- LABS_4_5/LABS_4_5.srcs/sim_1/new/TB_ClockGen.vhd
+++ LABS_4_5/LABS_4_5.srcs/sim_1/new/TB_ClockGen.vhd
@@ -1,43 +1,70 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 30.10.2019 10:21:27
--- Design Name: 
--- Module Name: TB_ClockGen - RTL
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity TB_ClockGen is
---  Port ( );
-end TB_ClockGen;
-
-architecture RTL of TB_ClockGen is
-
-begin
-
-
-end RTL;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY Tb_clock_divider IS
+END ENTITY Tb_clock_divider;
+ 
+ARCHITECTURE RTL OF Tb_clock_divider IS
+ 
+-- Component Declaration for the Device Under Test (DUT)
+ 
+    COMPONENT ClockGen
+        GENERIC (
+            longPressState : STD_LOGIC := '0' );
+        PORT(
+            sysclk : IN std_logic;
+            Reset : IN std_logic;
+            clkout : OUT std_logic;
+            btn : IN std_logic_vector(3 downto 1);
+            output : OUT std_logic );
+    END COMPONENT ClockGen;
+ 
+--Inputs
+    signal sysclk : std_logic := '0';
+    signal Reset : std_logic := '0';
+    signal btn : std_logic_vector(3 downto 1) := (others => '0');
+ 
+--Outputs
+    signal clkout : std_logic;
+    signal output : std_logic;
+    signal longPress : std_logic;
+ 
+-- Clock period definitions
+    constant sysclk_period : time := 8 ns;
+ 
+BEGIN
+ 
+    -- Instantiate the Device Under Test (DUT)
+    DUT: ClockGen 
+        GENERIC MAP (
+            longPressState => longPress )
+        PORT MAP (
+            sysclk => sysclk,
+            Reset => Reset,
+            clkout => clkout,
+            btn => btn,
+            output => output );
+     
+    -- Clock process
+    sysclk_p :process
+        begin
+        sysclk <= '0';
+        wait for sysclk_period/2;
+        sysclk <= '1';
+        wait for sysclk_period/2;
+    end process sysclk_p;
+     
+    -- Stimulus process
+    stim_p: process
+        begin
+        wait for 100 ns;
+        Reset <= '1';
+        wait for 5 ms;
+        Reset <= '0';
+        btn(1) <= '1';
+        wait for 200 ns;
+        btn(1) <= '0';
+        wait;
+    end process stim_p;
+ 
+END ARCHITECTURE RTL;
