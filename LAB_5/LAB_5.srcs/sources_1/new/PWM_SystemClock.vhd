1d35a3134ca1144ad68ec993f04546444ed7aa54
pelthe <54885089+pelthe@users.noreply.github.com>
2019-11-06 09:55:39 +0200

    Created LAB5 project

---
+++ LAB_5/LAB_5.srcs/sources_1/new/PWM_SystemClock.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06.11.2019 09:51:35
+-- Design Name: 
+-- Module Name: PWM_SystemClock - RTL
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PWM_SystemClock is
+--  Port ( );
+end PWM_SystemClock;
+
+architecture RTL of PWM_SystemClock is
+
+begin
+
+
+end RTL;
